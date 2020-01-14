d1bb040a8875069ff5a2d8883bd67e0c4f2dc308
Zhenkui Wang <815195861@qq.com>
2020-01-15 00:13:57 +0800

    确实

--- fanli-api/src/main/java/com/datu/fanli/service/IProductService.java
+++ fanli-api/src/main/java/com/datu/fanli/service/IProductService.java
@@ -1,9 +1,18 @@
 package com.datu.fanli.service;
 
+import com.datu.fanli.bean.ProductInfo;
+import com.datu.fanli.bean.ProductSaleAttr;
+
+import java.util.List;
+
 /**
  * @author: datu
  * @date: 2020/1/14
  */
 public interface IProductService {
+    List<ProductInfo> getProductList();
 
+    int saveProductInfo(ProductInfo productInfo);
+
+    List<ProductSaleAttr> getBaseSaleAttrList();
 }
